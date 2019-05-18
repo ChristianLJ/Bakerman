6bb922a79970372b7f4a8a09b49fc51b47f5e0b9
Christian Leicht <christianlj@ltaps.dk>
2019-05-18 13:13:39 +0200

    Updated dependencies, and added Terser to Webpack, removing console.log from production

--- scripts/steps/build/typescript.js
+++ scripts/steps/build/typescript.js
@@ -4,6 +4,8 @@ const hash = require("../../utils/hash");
 const getBuildVars = require("../../utils/get-build-vars");
 const webpack = require("webpack");
 const path = require("path");
+const TerserPlugin = require('terser-webpack-plugin');
+
 /**
  * @param {string} baseDir
  * @param {string} relativeFile
@@ -14,14 +16,22 @@ module.exports = function run(baseDir, mode) {
         run() {
             l.info("Starting typescript compilation process in mode: " + mode);
 
-            const input = path.resolve('./src/app/app.ts');
-            const output = 'app.min.js';
-            const webpackMode = mode === "DEV" ? "development": "production";
+            const webpackMode = mode === "DEV" ? "development" : "production";
 
-            //const compiler = webpack([{entry: input, output: {filename: output}}]);
+            const minimizerPlugins = [];
+            if (webpackMode === "production") {
+                minimizerPlugins.push(new TerserPlugin({
+                    sourceMap: true, // Must be set to true if using source-maps in production
+                    terserOptions: {
+                        compress: {
+                            drop_console: true,
+                        },
+                    },
+                }),);
+            }
 
             const compiler = webpack({
-                mode: webpackMode,
+                    mode: webpackMode,
                     entry: './src/app/app.ts',
                     module: {
                         rules: [
@@ -52,7 +62,9 @@ module.exports = function run(baseDir, mode) {
                     output: {
                         filename: 'app.min.js',
                         path: path.resolve('./dist')
-                    }
+                    }, optimization: {
+                        minimizer: minimizerPlugins,
+                    },
                 }
             );
 
